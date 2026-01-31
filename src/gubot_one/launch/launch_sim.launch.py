f296754506c256a8f7a66097a8b40c40f751357c
goldjunge91 <30201929+goldjunge91@users.noreply.github.com>
2026-01-31 10:17:41 +0100

    feat: update robot_core.xacro and launch_sim.launch.py for improved package structure and world argument configuration

--- src/gubot_one/launch/launch_sim.launch.py
+++ src/gubot_one/launch/launch_sim.launch.py
@@ -14,18 +14,19 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    package_name='gubot_one' #<--- CHANGE ME
 
     # Declare the 'world' argument
     world_arg = DeclareLaunchArgument(
         'world',
-        default_value='',
+        # default_value='',
+        default_value=os.path.join(get_package_share_directory(package_name), 'worlds', 'obstacles.world'),
         description='World to load'
     )
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='gubot_one' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
