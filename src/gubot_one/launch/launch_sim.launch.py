6d9a178560439b724d6ad3259f0a78c26671aa90
goldjunge91 <30201929+goldjunge91@users.noreply.github.com>
2026-02-02 08:25:10 +0100

    feat: Add STL mesh analysis tool and improve simulation launch stability by delaying controller spawning.

--- src/gubot_one/launch/launch_sim.launch.py
+++ src/gubot_one/launch/launch_sim.launch.py
@@ -2,68 +2,97 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import (
+    IncludeLaunchDescription,
+    DeclareLaunchArgument,
+    RegisterEventHandler,
+)
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-    package_name='gubot_one' #<--- CHANGE ME
+    package_name = "gubot_one"
 
     # Declare the 'world' argument
     world_arg = DeclareLaunchArgument(
-        'world',
-        # default_value='',
-        default_value=os.path.join(get_package_share_directory(package_name), 'worlds', 'obstacles.world'),
-        description='World to load'
+        "world",
+        default_value=os.path.join(
+            get_package_share_directory(package_name), "worlds", "obstacles.world"
+        ),
+        description="World to load",
     )
 
-
-
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name), "launch", "rsp.launch.py"
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true", "use_ros2_control": "true"}.items(),
     )
 
+    # Joystick
     joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "joystick.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "true"}.items(),
     )
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    # Twist Mux
+    twist_mux_params = os.path.join(
+        get_package_share_directory(package_name), "config", "twist_mux.yaml"
+    )
     twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params, {'use_sim_time': True}],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params, {"use_sim_time": True}],
+        remappings=[("/cmd_vel_out", "/diff_cont/cmd_vel_unstamped")],
+    )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
-
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # Gazebo
+    gazebo_params_file = os.path.join(
+        get_package_share_directory(package_name), "config", "gazebo_params.yaml"
+    )
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'world': LaunchConfiguration('world'), 'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("gazebo_ros"),
+                    "launch",
+                    "gazebo.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={
+            "world": LaunchConfiguration("world"),
+            "extra_gazebo_args": "--ros-args --params-file " + gazebo_params_file,
+        }.items(),
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'gubot_one'],
-                        output='screen')
+    # Spawn Entity - This MUST complete before controllers can start
+    spawn_entity = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=["-topic", "robot_description", "-entity", "gubot_one"],
+        output="screen",
+    )
 
-
+    # Controller Spawners - Must wait for spawn_entity to complete
     diff_drive_spawner = Node(
         package="controller_manager",
         executable="spawner",
@@ -76,41 +105,69 @@ def generate_launch_description():
         arguments=["joint_broad"],
     )
 
-
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        arguments=['-d', os.path.join(get_package_share_directory(package_name), 'config', 'view_bot.rviz')],
-        output='screen'
+    # WICHTIG: Verzögerung der Controller bis spawn_entity fertig ist
+    delayed_diff_drive_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[diff_drive_spawner],
+        )
     )
 
-    # Code for delaying a node (I haven't tested how effective it is)
-    # 
-    # First add the below lines to imports
-    # from launch.actions import RegisterEventHandler
-    # from launch.event_handlers import OnProcessExit
-    #
-    # Then add the following below the current diff_drive_spawner
-    # delayed_diff_drive_spawner = RegisterEventHandler(
-    #     event_handler=OnProcessExit(
-    #         target_action=spawn_entity,
-    #         on_exit=[diff_drive_spawner],
-    #     )
-    # )
-    #
-    # Replace the diff_drive_spawner in the final return with delayed_diff_drive_spawner
+    delayed_joint_broad_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[joint_broad_spawner],
+        )
+    )
 
+    # Nerf Launcher Controllers - auch verzögern
+    nerf_controllers = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("nerf_dart_launcher"),
+                    "launch",
+                    "nerf_controllers.launch.py",
+                )
+            ]
+        ),
+    )
 
+    delayed_nerf_controllers = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_entity,
+            on_exit=[nerf_controllers],
+        )
+    )
 
-    # Launch them all!
-    return LaunchDescription([
-        world_arg,
-        rsp,
-        joystick,
-        twist_mux,
-        gazebo,
-        spawn_entity,
-        diff_drive_spawner,
-        joint_broad_spawner,
-        rviz_node
-    ])
+    # RViz
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=[
+            "-d",
+            os.path.join(
+                get_package_share_directory(package_name), "config", "view_bot.rviz"
+            ),
+        ],
+        output="screen",
+    )
+
+    # Launch mit korrekter Reihenfolge:
+    # 1. Gazebo + RSP starten gleichzeitig
+    # 2. spawn_entity startet (wartet intern auf /spawn_entity service)
+    # 3. NACH spawn_entity: Controller starten
+    return LaunchDescription(
+        [
+            world_arg,
+            rsp,
+            joystick,
+            twist_mux,
+            gazebo,
+            spawn_entity,
+            delayed_diff_drive_spawner,
+            delayed_joint_broad_spawner,
+            delayed_nerf_controllers,
+            rviz_node,
+        ]
+    )
