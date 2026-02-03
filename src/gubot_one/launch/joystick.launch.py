b4c7895fd70d9f94fe1eb93df641825095a8d680
goldjunge91 <30201929+goldjunge91@users.noreply.github.com>
2026-02-03 01:56:52 +0100

    feat: Update serial commands for hardware deactivation and improve code formatting.

--- src/gubot_one/launch/joystick.launch.py
+++ src/gubot_one/launch/joystick.launch.py
@@ -2,6 +2,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+import launch.conditions as if_condition
 
 import os
 from ament_index_python.packages import get_package_share_directory
@@ -9,6 +11,8 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time")
+
+    launch_joy_node = LaunchConfiguration("launch_joy_node")
 
     joy_params = os.path.join(
         get_package_share_directory("gubot_one"), "config", "joystick.yaml"
@@ -18,6 +22,7 @@ def generate_launch_description():
         package="joy",
         executable="joy_node",
         parameters=[joy_params, {"use_sim_time": use_sim_time}],
+        condition=if_condition.IfCondition(launch_joy_node),
     )
 
     teleop_node = Node(
@@ -49,6 +54,11 @@ def generate_launch_description():
                 "use_sim_time",
                 default_value="false",
                 description="Use sim time if true",
+            ),
+            DeclareLaunchArgument(
+                "launch_joy_node",
+                default_value="true",
+                description="Whether to run joy_node locally",
             ),
             joy_node,
             teleop_node,
--- src/gubot_one/launch/launch_robot.launch.py
+++ src/gubot_one/launch/launch_robot.launch.py
@@ -30,11 +30,18 @@ def generate_launch_description():
         launch_arguments={"use_sim_time": "false", "use_ros2_control": "true"}.items(),
     )
 
-    # joystick = IncludeLaunchDescription(
-    #             PythonLaunchDescriptionSource([os.path.join(
-    #                 get_package_share_directory(package_name),'launch','joystick.launch.py'
-    #             )])
-    # )
+    joystick = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory(package_name),
+                    "launch",
+                    "joystick.launch.py",
+                )
+            ]
+        ),
+        launch_arguments={"use_sim_time": "false", "launch_joy_node": "false"}.items(),
+    )
 
     twist_mux_params = os.path.join(
         get_package_share_directory(package_name), "config", "twist_mux.yaml"
@@ -165,7 +172,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             rsp,
-            # joystick,
+            joystick,
             twist_mux,
             delayed_controller_manager,
             delayed_diff_drive_spawner,
