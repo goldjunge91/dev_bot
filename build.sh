34d6c76cd874645a9d562622fabba0a802a7880d
goldjunge91 <30201929+goldjunge91@users.noreply.github.com>
2026-02-25 07:45:41 +0100

    feat: Configure VS Code for Linux ROS development by updating C/C++ IntelliSense settings, extending Python analysis paths, and enabling compile command generation.

--- build.sh
+++ build.sh
@@ -6,9 +6,11 @@ source /opt/ros/humble/setup.bash
 
 # Falls du ein spezielles Build-Profil (Debug) übergeben willst
 if [ "$BUILD_TYPE" == "Debug" ]; then
-    colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Debug
+    # OLD: colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Debug
+    colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 else
-    colcon build --symlink-install
+    # OLD: colcon build --symlink-install
+    colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 fi
 
 source install/setup.bash
