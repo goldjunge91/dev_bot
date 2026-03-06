cf5b274ac203cb687a2dbaa189f7bac26fb3165c
goldjunge91 <30201929+goldjunge91@users.noreply.github.com>
2026-03-06 14:29:20 +0100

    refactor: comment out specific controller definitions and alternative launch configurations.

--- src/gubot_one/launch/face_tracker.launch.py
+++ src/gubot_one/launch/face_tracker.launch.py
@@ -47,6 +47,24 @@ def generate_launch_description():
         ]
     )
 
+    # tracker_launch = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         [
+    #             os.path.join(
+    #                 get_package_share_directory("face_tracker"),
+    #                 "launch",
+    #                 "face_tracker.launch.py",
+    #             )
+    #         ]
+    #     ),
+    #     launch_arguments={
+    #         "params_file": params_path,
+    #         "image_topic": "/camera/image_raw",
+    #         "cmd_vel_topic": "/cmd_vel_tracker",
+    #         "enable_3d_tracker": "true",
+    #     }.items(),
+    # )
+
     tracker_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [
